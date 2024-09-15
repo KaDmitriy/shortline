d50fd9e65531f2d8916e13c588181f318d5c7e99
KaDmitriy <ka.dmitriy@gmail.com>
2024-09-15 17:56:19 +0300

    changed docker build - added version check to build.sh

--- docker/content/build.sh
+++ docker/content/build.sh
@@ -1,5 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-VERSION=$1
+filename="rootfs/version"
+VERSION=0
+APPLICATIONNAME=""
+APPLICATIONFILE=""
 
-docker build --build-arg version=$VERSION -t shortline/content:$VERSION .
+function parseline {
+	#echo "line=$1 $2"
+	if [ "$1" = "Version:" ]; then
+		VERSION=$2
+	fi
+}
+
+while read -r line; do
+    name="$line"
+    parseline $line
+done < "$filename"
+
+if [ "$VERSION" != "0" ]; then
+    echo "VERSION=$VERSION"
+    docker build --build-arg version=$VERSION -t shortline/content:$VERSION .
+else
+   echo "ERROR: unspecified version!"
+fi
+
+
